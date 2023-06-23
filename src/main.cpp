6dccee0b4afa16f22c11df8347d744d3ce70476e
Felix Quehl <felix.quehl@icloud.com>
2023-06-23 15:06:41 +0200

    rewrote main to use test class

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
-#include <hello_world.h>
 #include <cassert>
 
-void test_message_is_correct()
-{
-    std::string expected = "Hello World & Hello Eleven!";
-    std::string myName = "Eleven";
-    Examples::HelloWorld helloWorld(myName);
-
-    std::string actual = helloWorld.getMessage();
-    
-    assert(expected == actual);
-}
+#include <stringUtilityTests.h>
 
 int main()
 {
-    test_message_is_correct();
-    std::cout << "all tests passed" << std::endl;
+    CodingChallenge::StringUtilityTests stringUtilityTests;
+    stringUtilityTests.test_all();
     return 0;
 }
