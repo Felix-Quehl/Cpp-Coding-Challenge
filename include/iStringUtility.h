3ec789d50600613482e28ab2f823f71958493b52
Felix Quehl <felix.quehl@icloud.com>
2023-06-23 15:05:39 +0200

    added interface

---
+++ include/iStringUtility.h
@@ -0,0 +1,21 @@
+#ifndef HEADER_IStringUtility
+#define HEADER_IStringUtility
+
+#include <string>
+
+namespace CodingChallenge
+{
+
+    struct InterpolationPair
+    {
+        std::string key;
+        std::string value;
+    };
+
+    class IStringUtility
+    {
+    public:
+        virtual std::string interpolate(std::string *format, InterpolationPair *values, int count) = 0;
+    };
+}
+#endif
