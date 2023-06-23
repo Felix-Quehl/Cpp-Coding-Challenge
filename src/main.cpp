708ee8a36541a4ece7ffcacf56065d43e95cc608
Felix Quehl <felix.quehl@icloud.com>
2023-06-23 15:53:36 +0200

    added comment about testing framework

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 
 int main()
 {
+    // If the coding challenge would not have been asking to implement in C++98,
+    // I would have used Catch as a Testing Framework, but they are on later standards.
+    // So I have swiftly thrown together some vanilla testing code.
     CodingChallenge::StringUtilityTests stringUtilityTests;
     stringUtilityTests.test_all();
     return 0;
